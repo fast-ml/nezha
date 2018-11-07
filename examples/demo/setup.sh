883b13e62d44b6ac1bde44464af145492a0183b3
Huamin Chen <hchen@redhat.com>
2018-11-07 19:42:55 +0000

    add readme and demo
    Signed-off-by: Huamin Chen <hchen@redhat.com>

---
+++ examples/demo/setup.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+
+NAMESPACE=${NAMESPACE:-"nezha-demo"}
+
+start() {
+    kubectl create ns ${NAMESPACE} || true
+    # create nginx config as a configmap
+    kubectl create -n ${NAMESPACE} configmap nginx-proxy --from-file=nginx.conf || true
+    # create nginx and svc
+    kubectl apply -n ${NAMESPACE} -f nginx.yaml
+    # create csr
+    ../../deploy/create-signed-crt.sh
+    # create webhook svc
+    CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 | tr -d '\n')
+    cat ../../deploy/mutatingwebhook.yaml | sed -e "s|\${CA_BUNDLE}|${CA_BUNDLE}|g" | kubectl apply -f -
+    # patch host aliases
+    SVC=$(kubectl get svc -n ${NAMESPACE} proxy-cache -o jsonpath={.spec.clusterIP})    
+    SERVERS=$(grep server_name nginx.conf |tr -d ';' |awk '{print $2}')
+    file=$(mktemp temp.XXX.yaml)
+    cat > ${file} <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: hostaliases-config
+data:
+  config: |
+      - name: dataset
+        label: dataset
+        hostAliases:
+        - ip: "${SVC}"
+          hostnames:
+EOF
+    for s in ${SERVERS}
+    do
+        echo "          - \"${s}\"" >> ${file}
+    done    
+    kubectl apply -f ${file}
+    rm ${file}
+}
+
+clean() {
+    kubectl delete -f ../../deploy/mutatingwebhook.yaml
+    kubectl delete  -n ${NAMESPACE} -f nginx.yaml
+    kubectl delete  -n ${NAMESPACE} configmap nginx-proxy
+    kubectl delete ns ${NAMESPACE}
+    exit 0
+}
+
+
+usage() {
+        cat <<EOF
+usage: ${0} start|clean
+EOF
+        exit 1
+}
+
+while [[ $# -gt 0 ]]
+do
+    case ${1} in
+        start)
+            start
+            ;;
+        clean)
+            clean
+            ;;
+        *)
+            usage
+            ;;
+    esac
+    shift
+done
+
+    
